566c9491d52d18ef9f8ae6680d3a206d02a39402
mtc2013 <mtc2013opensource@gmail.com>
2014-02-14 13:21:05 -0500

    Change selenium config to use firefox rather than chrome for travis

--- myConf.js
+++ myConf.js
@@ -3,10 +3,10 @@ exports.config = {
   // The address of a running selenium server.
   seleniumServerJar: 'node_modules/protractor/selenium/selenium-server-standalone-2.39.0.jar',
   seleniumPort: 4444,
-  chromeDriver: 'node_modules/protractor/selenium/chromedriver',
+  //chromeDriver: 'node_modules/protractor/selenium/chromedriver',
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'firefox'
   },
 
   // Spec patterns are relative to the location of the spec file. They may
